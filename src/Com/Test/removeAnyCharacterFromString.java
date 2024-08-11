cbe2fba245f55f96818fe0b96d8375d16facf743
Tejas <tdbhat007@gmail.com>
2024-08-11 18:07:05 +0530

    add program

---
+++ src/Com/Test/removeAnyCharacterFromString.java
@@ -0,0 +1,20 @@
+package Com.Test;
+
+public class removeAnyCharacterFromString {
+
+	public static void main(String[] args) {
+		String str= "Tejas";
+		char ch= 'T';
+		String op="";
+		
+		for(char c:str.toCharArray()){
+			if(c!=ch){
+				op=op+Character.toString(c);
+			}
+		}
+		
+		System.out.println(op);
+
+	}
+
+}
